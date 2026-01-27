f38dc8f3adebc8795a0a4cb45cc171310d428587
sangjin <sangjincha719@gmail.com>
2026-01-27 11:30:13 +0900

    Initial Commit

---
+++ RandomNotiApp/RandomNotiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomNotiAppApp.swift
+//  RandomNotiApp
+//
+//  Created by 김무경 on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct RandomNotiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
